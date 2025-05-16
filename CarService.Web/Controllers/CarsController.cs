b49ed94700f0008158ba76cadc0309d461c9cd86
Tobias Johansson <tobias@noq.nu>
2025-05-16 11:51:49 +0200

    Add car management features to CarsController
    Implemented Index and Add actions in CarsController for
    displaying and adding cars. Created Add.cshtml for the
    car addition form and updated Index.cshtml to list all
    cars with an option to add a new car.

--- CarService.Web/Controllers/CarsController.cs
+++ CarService.Web/Controllers/CarsController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CarService.Web.Services;
+using CarService.Web.Models;
 
 namespace CarService.Web.Controllers;
 public class CarsController : Controller
 {
     public static CarServices service = new CarServices();
+    [HttpGet("")]
     public IActionResult Index()
     {
+        return View(service.GetAllCars());
+    }
+    [HttpGet("add")]
+    public IActionResult Add()
+    {
         return View();
+    }
+    [HttpPost("add")]
+    public IActionResult Add(Car car)
+    {
+        if (!ModelState.IsValid)
+            return View(car);
+
+        return RedirectToAction(nameof(Index));
     }
 }
