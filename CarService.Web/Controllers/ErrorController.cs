67d9d95e7e2990bd01868ffcdfa70891c9b23540
Tobias Johansson <tobias@noq.nu>
2025-05-22 14:48:15 +0200

    Add error handling and logging features
    - Implemented `ErrorController` for handling server and HTTP errors.
    - Introduced `LogFilter` to log action execution warnings.
    - Updated `Program.cs` to register `LogFilter` and configure error handling middleware.
    - Created `HttpError.cshtml` for user-friendly error messages.
    - Enhanced `ServerError.cshtml` with a simple error message.

---
+++ CarService.Web/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarService.Web.Controllers;
+public class ErrorController : Controller
+{
+    [HttpGet("error/exception")]
+    public IActionResult ServerError()
+    {
+        return View();
+    }
+    [HttpGet("error/http/{status}")]
+    public IActionResult HttpError(int status)
+    {
+        return View(status);
+    }
+}
